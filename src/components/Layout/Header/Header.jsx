2f484bfc673b863afb7b6bf969984e7eeaef4200
Maxime Danic <maxime.danic@gmail.com>
2024-07-11 09:48:40 +0300

    feat: Add SportSeeLogo component to the header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,13 @@
 import styles from '../../../styles/components/Layout/Header/Header.module.scss';
+import SportSeeLogo from '../../reusable-UI/SportSeeLogo.jsx';
 import NavLinks from './NavLinks.jsx';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.wrapperLogo}></div>
+      <div className={styles.wrapperLogo}>
+        <SportSeeLogo />
+      </div>
       <NavLinks />
     </header>
   );
