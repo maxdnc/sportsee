40f89a361ecbea833cfb62e80a30a1222a2fcace
Maxime Danic <maxime.danic@gmail.com>
2024-07-11 09:49:07 +0300

    refactor: Update Layout component to include SideMenu and rearrange main content

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import SideMenu from './SideMenu/SideMenu';
 
 const Layout = () => {
   return (
     <div className="site">
       <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <div className="layoutDashBoard">
+        <SideMenu />
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
