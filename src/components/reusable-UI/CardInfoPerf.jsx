05483a50f989e1db28b6f0fdb89504212230bd96
Maxime Danic <maxime.danic@gmail.com>
2024-07-11 10:24:09 +0300

    feat: Add CardInfoPerf component to display performance information on cards

---
+++ src/components/reusable-UI/CardInfoPerf.jsx
@@ -0,0 +1,15 @@
+import styles from '../../styles/components/reusable-UI/CardInfoPerf.module.scss';
+import caloriesLogo from '../../../public/images/svg/caloriesLogo.svg';
+
+const CardInfoPerf = () => {
+  return (
+    <div className={styles.card}>
+      <img src={caloriesLogo} alt="placeholder" aria-hidden={true} />
+      <div className={styles.cardContent}>
+        <p className={styles.stat}>Titre</p>
+        <p className={styles.description}>Contenu</p>
+      </div>
+    </div>
+  );
+};
+export default CardInfoPerf;
