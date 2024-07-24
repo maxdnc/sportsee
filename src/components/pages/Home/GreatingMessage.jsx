75a4845c6225c212acb01928db81fa2bcbefff20
Maxime Danic <maxime.danic@gmail.com>
2024-07-24 16:23:34 +0300

    feat: Update GreatingMessage component to dynamically fetch and display user's first name

--- src/components/pages/Home/GreatingMessage.jsx
+++ src/components/pages/Home/GreatingMessage.jsx
@@ -1,9 +1,34 @@
 import styles from '../../../styles/pages/Home/GreatingMessage.module.scss';
-const GreatingMessage = () => {
+import { sessionUser } from '../../../services/api';
+import { useEffect, useState } from 'react';
+
+const GreatingMessage = ({ id }) => {
+  const [userFirstName, setUserFirstName] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const result = await sessionUser(id);
+        setUserFirstName(result.data.userInfos.firstName);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className={styles.wrapperText}>
       <h1 className={styles.title}>
-        Bonjour <span>Thomas</span>
+        Bonjour <span>{userFirstName}</span>
       </h1>
       <p className={styles.description}>
         Félicitation ! Vous avez explosé vos objectifs hier{' '}
