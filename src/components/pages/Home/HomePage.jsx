a39bbd5a5937bb88f2ccadb7971a910f4e718ae3
Maxime Danic <maxime.danic@gmail.com>
2024-07-26 15:55:02 +0300

    feat: Add ScoreRadarChart and NutritionDashboard components to home page

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,19 +1,21 @@
-import CardInfoPerf from '../../reusable-UI/CardInfoPerf';
 import AverageSessionLineChart from '../../reusable-UI/Chart/AverageSessionLineChart';
 import DailyActivityChart from '../../reusable-UI/Chart/DailyActivityChart';
 import PerformanceRadarChart from '../../reusable-UI/Chart/PerfomanceRadarChart.jsx';
+import ScoreRadarChart from '../../reusable-UI/Chart/ScoreRadarChart.jsx';
+import NutritionDashboard from '../../reusable-UI/NutritionDashboard';
 
 import GreatingMessage from './GreatingMessage';
 
 const HomePage = () => {
-  let userId = 18;
+  const userId = 18;
   return (
     <>
       <GreatingMessage id={userId} />
-      <CardInfoPerf />
       <DailyActivityChart id={userId} />
       <AverageSessionLineChart id={userId} />
       <PerformanceRadarChart id={userId} />
+      <ScoreRadarChart id={userId} />
+      <NutritionDashboard id={userId} />
     </>
   );
 };
