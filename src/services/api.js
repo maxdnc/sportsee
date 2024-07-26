f0b29dc882e83d8da23c311a330fd4ea4f44f16f
Maxime Danic <maxime.danic@gmail.com>
2024-07-26 12:29:44 +0300

    feat: Add averageSession and performanceSession functions to api.js

--- src/services/api.js
+++ src/services/api.js
@@ -29,3 +29,35 @@ export const userActivity = async (id) => {
   }
   return data;
 };
+
+export const averageSession = async (id) => {
+  let data;
+  const url = `http://localhost:3000/user/${id}/average-sessions`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+  return data;
+};
+
+export const performanceSession = async (id) => {
+  let data;
+  const url = `http://localhost:3000/user/${id}/performance`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+  return data;
+};
