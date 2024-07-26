08bd423b3f43c3a940d41230909795d57560e663
Maxime Danic <maxime.danic@gmail.com>
2024-07-26 12:30:04 +0300

    feat: Add PerformanceRadarChart and AverageSessionLineChart components to home page

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,14 +1,19 @@
 import CardInfoPerf from '../../reusable-UI/CardInfoPerf';
+import AverageSessionLineChart from '../../reusable-UI/Chart/AverageSessionLineChart';
 import DailyActivityChart from '../../reusable-UI/Chart/DailyActivityChart';
+import PerformanceRadarChart from '../../reusable-UI/Chart/PerfomanceRadarChart.jsx';
+
 import GreatingMessage from './GreatingMessage';
 
 const HomePage = () => {
-  let userId = 12;
+  let userId = 18;
   return (
     <>
       <GreatingMessage id={userId} />
       <CardInfoPerf />
       <DailyActivityChart id={userId} />
+      <AverageSessionLineChart id={userId} />
+      <PerformanceRadarChart id={userId} />
     </>
   );
 };
