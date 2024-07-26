fc23c54a023ab305b73ed6b12569884ea00467ab
Maxime Danic <maxime.danic@gmail.com>
2024-07-26 15:53:34 +0300

    Refactor: make CardInfoPerf reusable with props

--- src/components/reusable-UI/CardInfoPerf.jsx
+++ src/components/reusable-UI/CardInfoPerf.jsx
@@ -1,15 +1,15 @@
 import styles from '../../styles/components/reusable-UI/CardInfoPerf.module.scss';
-import caloriesLogo from '/images/svg/caloriesLogo.svg';
 
-const CardInfoPerf = () => {
+const CardInfoPerf = ({ icon, stat, description }) => {
   return (
     <div className={styles.card}>
-      <img src={caloriesLogo} alt="placeholder" aria-hidden={true} />
+      <img src={icon} alt={description} aria-hidden={true} />
       <div className={styles.cardContent}>
-        <p className={styles.stat}>Titre</p>
-        <p className={styles.description}>Contenu</p>
+        <p className={styles.stat}>{stat}</p>
+        <p className={styles.description}>{description}</p>
       </div>
     </div>
   );
 };
+
 export default CardInfoPerf;
