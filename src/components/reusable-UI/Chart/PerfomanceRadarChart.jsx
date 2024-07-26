0c574f4ddfd35dff20d39a66434b73e4242c520f
Maxime Danic <maxime.danic@gmail.com>
2024-07-26 15:54:50 +0300

    feat: Refactor PerfomanceRadarChart component to use recharts library

--- src/components/reusable-UI/Chart/PerfomanceRadarChart.jsx
+++ src/components/reusable-UI/Chart/PerfomanceRadarChart.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react';
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  Radar,
+  RadarChart,
+  ResponsiveContainer,
+} from 'recharts';
 import { performanceSession } from '../../../services/api';
+import styles from '../../../styles/components/reusable-UI/Chart/PerfomanceRadarChart.module.scss';
 
 const PerfomanceRadarChart = ({ id }) => {
   const [sessions, setSessions] = useState([]);
@@ -11,7 +19,11 @@ const PerfomanceRadarChart = ({ id }) => {
       try {
         setLoading(true);
         const result = await performanceSession(id);
-        setSessions(result.data.data);
+        const formattedData = result.data.data.map((item) => ({
+          value: item.value,
+          kind: result.data.kind[item.kind],
+        }));
+        setSessions(formattedData);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -21,10 +33,35 @@ const PerfomanceRadarChart = ({ id }) => {
     fetchData();
   }, [id]);
 
-  console.log(sessions);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-  return <div>test</div>;
+  const formatPolarAngleAxis = (kind) => {
+    return kind.charAt(0).toUpperCase() + kind.slice(1);
+  };
+
+  return (
+    <div className={styles.chart}>
+      <ResponsiveContainer width="100%" height={250}>
+        <RadarChart cx="50%" cy="50%" outerRadius="70%" data={sessions}>
+          <PolarGrid radialLines={false} />
+          <PolarAngleAxis
+            dataKey="kind"
+            tickFormatter={formatPolarAngleAxis}
+            stroke="white"
+            tickLine={false}
+            style={{ fontSize: '10px', fontWeight: '500' }}
+          />
+          <Radar
+            name="Performance"
+            dataKey="value"
+            fill="#FF0101"
+            fillOpacity={0.7}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
+
 export default PerfomanceRadarChart;
