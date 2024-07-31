9494340f7aafd97c742a588caf491097ade78ae2
Maxime Danic <maxime.danic@gmail.com>
2024-07-31 22:56:29 +0300

    refactor: Update HomePage component to use useSessionUser hook for fetching user data

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -5,57 +5,9 @@ import ScoreRadarChart from '../../reusable-UI/Chart/ScoreRadarChart';
 import NutritionDashboard from '../../reusable-UI/NutritionDashboard';
 import GreetingMessage from './GreatingMessage.jsx';
 import styles from '../../../styles/pages/Home/HomePage.module.scss';
-import {
-  useSessionUser,
-  useUserActivity,
-  useAverageSession,
-  usePerformanceSession,
-} from '../../../hooks/useApiCall';
 
 const HomePage = () => {
   const userId = 12;
-  const {
-    data: userData,
-    loading: userLoading,
-    error: userError,
-  } = useSessionUser(userId);
-  const {
-    data: activityData,
-    loading: activityLoading,
-    error: activityError,
-  } = useUserActivity(userId);
-  const {
-    data: averageSessionsData,
-    loading: averageSessionsLoading,
-    error: averageSessionsError,
-  } = useAverageSession(userId);
-  const {
-    data: performanceData,
-    loading: performanceLoading,
-    error: performanceError,
-  } = usePerformanceSession(userId);
-
-  if (
-    userLoading ||
-    activityLoading ||
-    averageSessionsLoading ||
-    performanceLoading
-  ) {
-    return <div>Loading...</div>;
-  }
-
-  if (userError || activityError || averageSessionsError || performanceError) {
-    return (
-      <div>
-        Error:{' '}
-        {userError || activityError || averageSessionsError || performanceError}
-      </div>
-    );
-  }
-
-  if (!userData || !activityData || !averageSessionsData || !performanceData) {
-    return null;
-  }
 
   return (
     <>
