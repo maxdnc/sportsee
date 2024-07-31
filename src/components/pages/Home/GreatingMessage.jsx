1291f8828b45000df173a6bd5ff35c318f26d6b4
Maxime Danic <maxime.danic@gmail.com>
2024-07-31 22:55:35 +0300

    Refactor GreatingMessage component to use useSessionUser hook for fetching user data

--- src/components/pages/Home/GreatingMessage.jsx
+++ src/components/pages/Home/GreatingMessage.jsx
@@ -1,12 +1,24 @@
+import { useSessionUser } from '../../../hooks/useApiCall';
 import styles from '../../../styles/pages/Home/GreatingMessage.module.scss';
 
-const GreatingMessage = ({ user }) => {
-  if (!user) return null;
+const GreatingMessage = ({ userId }) => {
+  const {
+    data: userData,
+    loading: userLoading,
+    error: userError,
+  } = useSessionUser(userId);
+
+  if (userLoading) {
+    return <div>Loading...</div>;
+  }
+  if (userError) {
+    return <div>Error: {userError}</div>;
+  }
 
   return (
     <div className={styles.wrapperText}>
       <h1 className={styles.title}>
-        Bonjour <span>{user.userInfos.firstName}</span>
+        Bonjour <span>{userData.data.userInfos.firstName}</span>
       </h1>
       <p className={styles.description}>
         Félicitation ! Vous avez explosé vos objectifs hier{' '}
