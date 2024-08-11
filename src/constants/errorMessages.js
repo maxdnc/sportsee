7cb0bea3bd008662a3c7bbab73377bd557a90d50
Maxime Danic <maxime.danic@gmail.com>
2024-08-11 17:17:43 +0300

    refactor: Add error messages constants for API responses

---
+++ src/constants/errorMessages.js
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGES = {
+  400: 'La requête est incorrecte. Veuillez vérifier vos données et réessayer.',
+  401: "Vous n'êtes pas autorisé à accéder à cette ressource. Veuillez vous connecter.",
+  403: "Accès refusé. Vous n'avez pas les permissions nécessaires.",
+  404: 'La ressource demandée est introuvable. Veuillez réessayer plus tard.',
+  405: "La méthode demandée n'est pas autorisée. Veuillez réessayer plus tard.",
+  500: 'Erreur interne du serveur. Veuillez réessayer plus tard.',
+  502: 'Mauvaise passerelle. Veuillez réessayer plus tard.',
+  503: 'Service indisponible. Veuillez réessayer plus tard.',
+  504: 'Le délai de la passerelle a expiré. Veuillez réessayer plus tard.',
+  default:
+    "Une erreur inattendue s'est produite. Veuillez réessayer plus tard.",
+};
