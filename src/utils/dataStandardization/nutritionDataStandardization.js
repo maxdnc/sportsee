b34f077050a5dd2a340474a2791c0ad02cc13c26
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 00:13:35 +0300

    feat: Add nutrition data standardization module

---
+++ src/utils/dataStandardization/nutritionDataStandardization.js
@@ -0,0 +1,27 @@
+// src/data/nutritionData.js
+import caloriesIcon from '/images/svg/calories-icon.svg';
+import proteinIcon from '/images/svg/protein-icon.svg';
+import carbIcon from '/images/svg/carb-icon.svg';
+import fatIcon from '/images/svg/fat-icon.svg';
+
+export const nutritionData = [
+  {
+    icon: caloriesIcon,
+    key: 'calorieCount',
+    unit: 'kCal',
+    description: 'Calories',
+  },
+  {
+    icon: proteinIcon,
+    key: 'proteinCount',
+    unit: 'g',
+    description: 'Proteines',
+  },
+  {
+    icon: carbIcon,
+    key: 'carbohydrateCount',
+    unit: 'g',
+    description: 'Glucides',
+  },
+  { icon: fatIcon, key: 'lipidCount', unit: 'g', description: 'Lipides' },
+];
