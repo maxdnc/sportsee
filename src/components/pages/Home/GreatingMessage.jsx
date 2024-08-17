de3ddd164a353b3af8d3b6ddb1bf678f35568304
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 00:14:40 +0300

    refactor: Update GreatingMessage component to use props for data, loading, error, and custom message

--- src/components/pages/Home/GreatingMessage.jsx
+++ src/components/pages/Home/GreatingMessage.jsx
@@ -1,56 +1,27 @@
-import { useSessionUser, useUserActivity } from '../../../hooks/useApiCall';
 import styles from '../../../styles/pages/Home/GreatingMessage.module.scss';
 import ErrorMessage from '../../reusable-UI/ErrorMessage';
 import Loader from '../../reusable-UI/Loader';
 
-const GreatingMessage = ({ userId }) => {
-  const {
-    data: userData,
-    loading: userLoading,
-    error: userError,
-  } = useSessionUser(userId);
-
-  const {
-    data: activityData,
-    loading: activityLoading,
-    error: activityError,
-  } = useUserActivity(userId);
-
-  if (userLoading || activityLoading) {
+const GreatingMessage = ({ data, loading, error, customMessage }) => {
+  if (loading) {
     return <Loader />;
   }
-  if (userError || activityError) {
+  if (error) {
     return (
       <ErrorMessage
         message={
-          userError ||
-          activityError ||
-          'Une erreur est survenue. Veuillez réessayer plus tard.'
+          error || 'Une erreur est survenue. Veuillez réessayer plus tard.'
         }
       />
     );
   }
 
-  // Calculer la moyenne des calories brûlées
-  const sessions = activityData.data.sessions;
-  const totalCalories = sessions.reduce(
-    (sum, session) => sum + session.calories,
-    0
-  );
-  const averageCalories = totalCalories / sessions.length;
-
-  // Déterminer le message en fonction de la moyenne des calories brûlées
-  const performanceMessage =
-    averageCalories > 250
-      ? 'Félicitation ! Vous avez brûlé beaucoup de calories cette semaine 👏'
-      : 'Continuez comme ça ! Vous êtes sur la bonne voie 🚀';
-
   return (
     <div className={styles.wrapperText}>
       <h1 className={styles.title}>
-        Bonjour <span>{userData.data.userInfos.firstName}</span>
+        Bonjour <span>{data.userInfos.firstName}</span>
       </h1>
-      <p className={styles.description}>{performanceMessage}</p>
+      <p className={styles.description}>{customMessage}</p>
     </div>
   );
 };
