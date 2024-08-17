1e286fa06a165ea43c8253ff027e349c6cc47019
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 01:41:08 +0300

    refactor: Add UserToggle component for toggling between User 12 and User 18

---
+++ src/components/pages/Home/UserToggle.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import styles from '../../../styles/pages/Home/UserToggle.module.scss';
+import { UserContext } from '../../../context/UserContext.jsx';
+
+const UserToggle = () => {
+  const { userId, setUserId } = useContext(UserContext);
+
+  const toggleUser = () => {
+    setUserId(userId === 12 ? 18 : 12);
+  };
+
+  return (
+    <div className={styles['toggle-container']}>
+      <span
+        className={`${styles['user-label']} ${userId === 12 ? styles['active'] : ''}`}
+      >
+        User 12
+      </span>
+      <label className={styles['switch']}>
+        <input type="checkbox" checked={userId === 18} onChange={toggleUser} />
+        <span className={`${styles['slider']} ${styles['round']}`}></span>
+      </label>
+      <span
+        className={`${styles['user-label']} ${userId === 18 ? styles['active'] : ''}`}
+      >
+        User 18
+      </span>
+    </div>
+  );
+};
+
+export default UserToggle;
