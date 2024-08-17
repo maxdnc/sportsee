9dba5c4c0d9fb82cc661b7c418cfaeb9db3b6090
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 00:13:13 +0300

    refactor: Standardize user data in userDataStandardization.js

---
+++ src/utils/dataStandardization/userDataStandardization.js
@@ -0,0 +1,27 @@
+export function standardizeUserData(apiData) {
+  const { data } = apiData;
+  const score = data.score || data.todayScore || 0;
+  const scorePercentage = score * 100;
+  const dataScore = [
+    {
+      score: score,
+      scorePercentage: scorePercentage,
+    },
+  ];
+
+  return {
+    id: data.id,
+    userInfos: {
+      firstName: data.userInfos.firstName,
+      lastName: data.userInfos.lastName,
+      age: data.userInfos.age,
+    },
+    dataScore: dataScore,
+    keyData: {
+      calorieCount: data.keyData.calorieCount,
+      proteinCount: data.keyData.proteinCount,
+      carbohydrateCount: data.keyData.carbohydrateCount,
+      lipidCount: data.keyData.lipidCount,
+    },
+  };
+}
