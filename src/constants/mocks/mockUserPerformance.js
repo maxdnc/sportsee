af179093ae4c0b521b63f7b1fad9f84bdf8d7522
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 11:01:03 +0300

    feat: Add mockUserPerformance constant for storing user performance data

---
+++ src/constants/mocks/mockUserPerformance.js
@@ -0,0 +1,11 @@
+export const MOCK_USER_PERFORMANCE = {
+  userId: 1,
+  performanceData: [
+    { value: 80, kindName: 'Cardio' },
+    { value: 120, kindName: 'Énergie' },
+    { value: 20, kindName: 'Endurance' },
+    { value: 50, kindName: 'Force' },
+    { value: 100, kindName: 'Vitesse' },
+    { value: 90, kindName: 'Intensité' },
+  ],
+};
