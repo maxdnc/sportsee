025b73ce2757c7ceab60b8385885a3c3381e6b80
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 11:00:53 +0300

    feat: Add mockUserActivity constant for storing user activity data

---
+++ src/constants/mocks/mockUserActivity.js
@@ -0,0 +1,12 @@
+export const MOCK_USER_ACTIVITY = {
+  userId: 1,
+  sessions: [
+    { dayNumber: '1', kilogram: 70, calories: 200 },
+    { dayNumber: '2', kilogram: 71, calories: 210 },
+    { dayNumber: '3', kilogram: 72, calories: 220 },
+    { dayNumber: '4', kilogram: 73, calories: 230 },
+    { dayNumber: '5', kilogram: 74, calories: 240 },
+    { dayNumber: '6', kilogram: 75, calories: 250 },
+    { dayNumber: '7', kilogram: 76, calories: 260 },
+  ],
+};
