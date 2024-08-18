17e62d72c17f98a298bcc53d150fcc5419f2d07c
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 11:00:48 +0300

    feat: Add mockUserInfo constant for storing user information

---
+++ src/constants/mocks/mockUserInfo.js
@@ -0,0 +1,18 @@
+export const MOCK_USER_INFO = {
+  id: 1,
+  userInfos: {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+  },
+  dataScore: {
+    score: 0.12,
+    scorePercentage: 12,
+  },
+  keyData: {
+    calorieCount: 2000,
+    proteinCount: 150,
+    carbohydrateCount: 300,
+    lipidCount: 70,
+  },
+};
