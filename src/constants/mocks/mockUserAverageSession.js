34fb85317ff3a1f7a262838e9d97c46d2f0b3cf8
Maxime Danic <maxime.danic@gmail.com>
2024-08-18 11:00:58 +0300

    feat: Add mockUserAverageSession constant for storing user average session data

---
+++ src/constants/mocks/mockUserAverageSession.js
@@ -0,0 +1,12 @@
+export const MOCK_USER_AVERAGE_SESSION = {
+  userId: 1,
+  sessions: [
+    { day: 'L', sessionLength: 30 },
+    { day: 'M', sessionLength: 45 },
+    { day: 'M', sessionLength: 50 },
+    { day: 'J', sessionLength: 60 },
+    { day: 'V', sessionLength: 20 },
+    { day: 'S', sessionLength: 40 },
+    { day: 'D', sessionLength: 35 },
+  ],
+};
