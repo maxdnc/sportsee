9d08239b4bedcfc5cdca827058f6eec6a44036c9
Maxime Danic <maxime.danic@gmail.com>
2024-08-24 16:39:12 +0300

    feat: Add mockErrorMessage constant for storing mock error messages

---
+++ src/constants/mocks/mockErrorMessage.js
@@ -0,0 +1,5 @@
+export const MOCK_ERROR_MESSAGES = {
+  notFound: 'Les données pour cet utilisateur sont introuvables.',
+  invalidData: 'Les données sont invalides ou corrompues.',
+  default: "Une erreur inattendue s'est produite.",
+};
